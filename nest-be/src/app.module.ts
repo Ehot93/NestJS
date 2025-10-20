86ed94059dfe23202011f7571853a829e268e079
Vladislav_Dementyev <vlad3x@mail.ru>
2025-10-20 17:50:41 +0300

    delete mistake controller

--- nest-be/src/app.module.ts
+++ nest-be/src/app.module.ts
@@ -5,11 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { Top=pageController } from './top=page/top=page.controller';
 
 @Module({
   imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
-  controllers: [AppController, Top=pageController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
